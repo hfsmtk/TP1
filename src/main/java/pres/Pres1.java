e3f8b9c6de729d83e66388046b36d1b198fe24d3
hfsmtk <160251950+hfsmtk@users.noreply.github.com>
2025-10-05 15:20:18 +0200

    first commit

---
+++ src/main/java/pres/Pres1.java
@@ -0,0 +1,13 @@
+package pres;
+
+import dao.DaoImpl;
+import metier.MetierImpl;
+
+public class Pres1 {
+    public static void main(String[] args) {
+        DaoImpl d = new DaoImpl();
+        MetierImpl metier = new MetierImpl();
+        metier.setDao(d);
+
+    }
+}
